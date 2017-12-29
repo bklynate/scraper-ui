cf5e8d4652984748c30c558a1cfe3a17a1d53216
bklynate <nathanielcfa@gmail.com>
2017-12-29 01:05:22 -0500

    house cleaning

--- routes/scraperRoutes.js
+++ routes/scraperRoutes.js
@@ -6,17 +6,17 @@ const scraper = new Aniscrape();
 
 module.exports = app => {
   app.post('/api/scrapeAnime', (request, response) => {
-    const { animeName } = request.body
+    const { animeName } = request.body;
     scraper.use(animebam).then(() => {
-      scraper.search(animeName, 'animebam').then((results) => {
-        scraper.fetchSeries(results[0]).then((anime) => {
+      scraper.search(animeName, 'animebam').then(results => {
+        scraper.fetchSeries(results[0]).then(anime => {
           const { url } = anime.episodes[0];
           xray(url, 'iframe.embed-responsive-item@src')((error, info) => {
             const payload = {
               animeName,
               info
-            }
-            response.send(payload)
+            };
+            response.send(payload);
           });
         });
       });
