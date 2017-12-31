cc2de4473ce5a18d016a596bcc34a50547692cf5
bklynate <nathanielcfa@gmail.com>
2017-12-30 20:53:23 -0500

    adds handling for single search result

--- routes/scraperRoutes.js
+++ routes/scraperRoutes.js
@@ -11,6 +11,7 @@ module.exports = app => {
       scraper.search(animeName, 'animebam').then(results => {
         if (results.length < 1) return;
         if (results.length > 1) return response.send(results); // eslint-disable-line
+        return response.send(results); // eslint-disable-line
       });
     });
   });
