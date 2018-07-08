cae105fb54fb37f4171663aa1e48893e19028a14
bklynate <nathanielcfa@gmail.com>
2018-07-08 13:51:57 -0400

    fix

---
+++ server/config/dev.js
@@ -0,0 +1,8 @@
+// Dev.js
+import devKeys from './devKeys.json';
+
+export default {
+  googleClientID: devKeys.googleClientID,
+  googleClientSecret: devKeys.googleClientSecret,
+  cookieKey: devKeys.cookieKey
+};
