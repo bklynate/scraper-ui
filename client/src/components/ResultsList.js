d9651113e8518c0348317d1415a890f85e06678d
bklynate <nathanielcfa@gmail.com>
2018-08-29 22:01:00 -0400

    house cleaning

--- client/src/components/ResultsList.js
+++ client/src/components/ResultsList.js
@@ -5,50 +5,56 @@ import { BeatLoader } from 'react-spinners';
 import ResultItem from './ResultItem';
 
 class ResultsList extends Component {
-  state = {
-    loading: true,
-  };
+	state = {
+		loading: false,
+	};
 
-  shouldComponentUpdate = (nextProps, nextState) => {
-    return this.props.anime !== nextProps.anime;
-  };
+	shouldComponentUpdate = (nextProps, nextState) => {
+		return this.props.anime !== nextProps.anime;
+	};
 
-  componentWillReceiveProps = nextProps => {
-    const { anime: nextAnimeList } = nextProps;
-    const { anime: currentAnimeList } = this.props;
-    if (nextAnimeList !== currentAnimeList) this.setState({ loading: false });
-  };
+	componentWillReceiveProps = (nextProps) => {
+		const { anime: nextAnimeList } = nextProps;
+		const { anime: currentAnimeList } = this.props;
+		if (nextAnimeList !== currentAnimeList) this.setState({ loading: false });
+	};
 
-  renderResultsList = () => {
-    const { loading } = this.state;
-    const { anime: animeArray = [] } = this.props || {};
-    if (loading)
-      return <BeatLoader size={20} margin="20px" color={'#EC6F75'} />;
-    if (animeArray.length) {
-      return animeArray.map(({ seriesName, seriesUrl }, index) => (
-        <ResultItem
-          key={index}
-          id={index}
-          seriesName={seriesName}
-          seriesUrl={seriesUrl}
-        />
-      ));
-    }
-  };
+	componentDidUpdate = (prevProps, prevState) => {
+		console.log('inside componentDidUpdate::', prevProps, prevState);
+	};
 
-  render() {
-    return (
-      <div className="results-list">
-        {this.renderResultsList()}
-      </div>
-    );
-  }
+	renderResultsList = () => {
+		const { loading } = this.state;
+		this.setState({ loading: true });
+		const { anime: animeArray = [] } = this.props || {};
+
+		if (animeArray.length) {
+			return animeArray.map(({ seriesName, seriesUrl }, index) => (
+				<ResultItem key={index} id={index} seriesName={seriesName} seriesUrl={seriesUrl} />
+			));
+
+			if (loading) return <BeatLoader size={20} margin="20px" color={'#EC6F75'} />;
+		}
+	};
+
+	render() {
+		const { loading } = this.state;
+		console.log('loading::', loading);
+		console.log(this.props.anime.length);
+
+		return (
+			<div className="results-list">
+				{this.props.anime.length ? '' : <h3>0 search results...</h3>}
+				{this.renderResultsList()}
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = ({ anime = [] }) => {
-  return {
-    anime,
-  };
+	return {
+		anime,
+	};
 };
 
 export default connect(mapStateToProps)(ResultsList);
--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleID: String
+	googleID: String,
 });
 
 mongoose.model('users', userSchema);
