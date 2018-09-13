c4bfa889e5733954cf91d1d23cc9bd2b12fd0b92
bklynate <nathanielcfa@gmail.com>
2018-09-12 20:24:10 -0400

    house cleaning

--- client/src/components/AnimePage.js
+++ client/src/components/AnimePage.js
@@ -6,97 +6,78 @@ import VideoPlayer from './VideoPlayer';
 import * as actions from './../actions';
 
 class AnimePage extends Component {
-  state = {
-    videoSrc: '',
-    loading: true,
-  };
+	state = {
+		videoSrc: '',
+	};
 
-  async componentDidMount() {
-    const { id: idx = '' } = this.props.match.params || {};
-    const { animeList = [] } = this.props.data;
-    console.log('this is animeList::', animeList);
-    const animeEpisodeToFetch = animeList[idx];
-    console.log('this is animeEpisodeToFetch::', animeEpisodeToFetch);
+	async componentDidMount () {
+		const { id: idx = '' } = this.props.match.params || {};
+		const { animeList = [] } = this.props.data || {};
+		const animeEpisodeToFetch = animeList[idx];
 
-    if (animeEpisodeToFetch === undefined)
-      return <Redirect to="/searchAnime" />;
+		if (animeEpisodeToFetch === undefined) return <Redirect to='/searchAnime' />;
 
-    await this.props.fetchAnimeEpisode(animeEpisodeToFetch);
-  }
+		await this.props.fetchAnimeEpisode(animeEpisodeToFetch);
+	}
 
-  shouldComponentUpdate = (nextProps, nextState) => {
-    const { episodeListData: { episodeList: nextEpisodeList = [] } = {} } = nextProps || {}
-    const { episodeListData: { episodeList: currentEpisodeList = [] } = {} } = this.props || {}
-    return (
-      currentEpisodeList !== nextEpisodeList ||
-      this.state.videoSrc !== nextState.videoSrc
-    );
-  };
+	shouldComponentUpdate = (nextProps, nextState) => {
+		const { episodeListData: { episodeList: nextEpisodeList = [] } = {} } = nextProps || {};
+		const { episodeListData: { episodeList: currentEpisodeList = [] } = {} } = this.props || {};
+		return currentEpisodeList !== nextEpisodeList || this.state.videoSrc !== nextState.videoSrc;
+	};
 
-  componentWillReceiveProps = nextProps => {
-    const { episodeListData: { episodeList: nextEpisodeList = [] } = {} } = nextProps || {}
-    const { episodeListData: { episodeList: currentEpisodeList = [] } = {} } = this.props || {}
-    if (nextEpisodeList !== currentEpisodeList) this.setState({ loading: false });
-  };
+	getVideoSrc = e => {
+		const videoSrc = e.currentTarget.attributes[0].textContent.replace('https://', 'https://www.');
+		this.setState({ videoSrc });
+	};
 
-  getVideoSrc = e => {
-    const videoSrc = e.currentTarget.attributes[0].textContent.replace(
-      'https://',
-      'https://www.',
-    );
-    this.setState({ videoSrc });
-  };
+	renderAnimeTitle = () => {
+		// this pulls the id/index of the animeTitle the user clicked on
+		const { id: idx = '' } = this.props.match.params || {};
+		const { animeList = [] } = this.props.data || {};
+		const { seriesName = '' } = animeList[idx];
+		return seriesName;
+	};
 
-  renderAnimeTitle = () => {
-    // this pulls the id/index of the animeTitle the user clicked on
-    const { id: idx = '' } = this.props.match.params || {};
-    const { animeList = [] } = this.props.data || {};
-    const { seriesName = '' } = animeList[idx];
-    return seriesName;
-  };
+	renderAnimeEpisodeList = () => {
+		const { loading = false } = this.props.episodeListData || {};
+		const { episodeListData: { episodeList = [] } = {} } = this.props || {};
 
-  renderAnimeEpisodeList = () => {
-    const { loading } = this.state;
-    const { episodeListData: { episodeList = [] } = {} } = this.props || {}
-    if (loading) return <BounceLoader size={60} color={'#EC6F75'} />;
-    if (episodeList.length) {
-      return episodeList.map((episode, index) => (
-        <div key={index} className="chip">
-          <a value={episode} onClick={this.getVideoSrc}>
-            {`Episode: ${index + 1}`}
-          </a>
-        </div>
-      ));
-    }
-  };
+		if (loading) return <BounceLoader size={60} color={'#EC6F75'} />;
 
-  render() {
-    const { videoSrc } = this.state;
-    const { id: idx = '' } = this.props.match.params || {};
-    const { animeList = [] } = this.props.data;
-    const animeEpisode = animeList[idx];
-    return (
-      <div>
-        {
-          animeEpisode === undefined
-          ? <Redirect to="/searchAnime" />
-          : <h1>{this.renderAnimeTitle()}</h1>
-        }
+		return episodeList.map((episode, index) => (
+			<div key={index} className='chip'>
+				<a value={episode} onClick={this.getVideoSrc}>
+					{`Episode: ${index + 1}`}
+				</a>
+			</div>
+		));
+	};
 
-        <div className="animePage-list">
-          {this.renderAnimeEpisodeList()}
-        </div>
-        <VideoPlayer videoSrc={videoSrc} />
-      </div>
-    );
-  }
+	render () {
+		const { videoSrc } = this.state;
+		const { id: idx = '' } = this.props.match.params || {};
+		const { animeList = [] } = this.props.data;
+		const animeEpisode = animeList[idx];
+
+		return (
+			<div>
+				{animeEpisode === undefined ? (
+					<Redirect to='/searchAnime' />
+				) : (
+					<h1>{this.renderAnimeTitle()}</h1>
+				)}
+
+				<div className='animePage-list'>{this.renderAnimeEpisodeList()}</div>
+				<VideoPlayer videoSrc={videoSrc} />
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = ({ animeData: data = {}, episodeListData = {} }) => {
-  return {
-    data,
-    episodeListData,
-  };
-};
+const mapStateToProps = ({ animeData: data = {}, episodeListData = {} }) => ({
+	data,
+	episodeListData,
+});
 
 export default connect(mapStateToProps, actions)(AnimePage);
